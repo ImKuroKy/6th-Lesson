9b13b344e244595004706ecb0e5d4b15d14e877f
ImKuroKy <spiritofthesovietunion@gmail.com>
2022-08-21 03:24:38 +0400

    Ещё парочка улучшений

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,6 @@ function Box (x, y, z, w) {
     this.depth = z;
     this.material = w;
 };
-
 Box.prototype.volume = function () {
     return ("Объём коробки равен " + (this.height * this.width * this.depth) + "<br/>");
 };
@@ -16,24 +15,14 @@ Box.prototype.equals = function (otherObj) {
     let answer = (this.material === otherObj.material) ? "да" : "нет";
     return "Коробки из одного материала? Ответ: " + answer + "<br/>";
 };
-
 var firstBox = new Box(5, 5, 5, "cardboard");
 var secondBox = new Box(7, 3, 5, "paper");
 var thirdBox = new Box(2, 2, 10, "plastic");
 var fourthBox = new Box(6, 4, 2, "cardboard");
-
-var res = firstBox.volume();
-document.write(res);
-res = secondBox.volume();
-document.write(res);
-res = thirdBox.volume();
-document.write(res);
-res = fourthBox.volume();
-document.write(res);
-
-res = firstBox.equals(secondBox);
-document.write(res);
-res = firstBox.equals(thirdBox);
-document.write(res);
-res = firstBox.equals(fourthBox);
-document.write(res);
+document.write(firstBox.volume());
+document.write(secondBox.volume());
+document.write(thirdBox.volume());
+document.write(fourthBox.volume());
+document.write(firstBox.equals(secondBox));
+document.write(firstBox.equals(thirdBox));
+document.write(firstBox.equals(fourthBox));
